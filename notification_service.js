2152974dfa67e9fff01be44c2d6fa5ab6736b5a6
Diwa del Mundo <diwadm@gmail.com>
2015-04-06 00:49:24 +0800

    Stubbing example.

--- notification_service.js
+++ notification_service.js
@@ -1,12 +1,16 @@
 module.exports = function() {
   return {
+    todayDate: function() {
+      return new Date();
+    },
+
     sendGreeting: function(user) {
-      var today = new Date();
+      var today = this.todayDate();
       var birthday = user.birthdate();
 
       if(birthday.getMonth() === today.getMonth() &&
         birthday.getDate() === today.getDate()) {
-// Send greeting.
+        // Send greeting.
         console.log('Happy birthday!');
         return true;
       }
--- test/notification_service.test.js
+++ test/notification_service.test.js
@@ -1,5 +1,6 @@
 var NotifierService = require('../notification_service');
 var should = require('chai').should();
+var sinon = require('sinon');
 
 describe('NotifierService', function() {
   var ns, user;
@@ -14,8 +15,21 @@ describe('NotifierService', function() {
   });
 
   describe('#sendGreeting()', function() {
-    it('should be false', function() {
-      ns.sendGreeting(user).should.to.be.false;
+    context('when the user birthday is today', function() {
+      var stub, stubSendGreeting;
+      before(function() {
+        stubSendGreeting = sinon.stub(ns, "todayDate");
+        stubSendGreeting.returns(new Date("01/14/1985"));
+      });
+
+      it('should be true', function() {
+        ns.sendGreeting(user).should.be.true;
+      });
+
+      after(function() {
+        stubSendGreeting.restore();
+      });
     });
+
   });
 });
